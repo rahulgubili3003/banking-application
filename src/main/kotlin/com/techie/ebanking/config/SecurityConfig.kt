8a2f60373fcbabc9b779f1bc450a90392493e48a
rahulgubili <rahul.gubiligari@paypay-corp.co.jp>
2024-06-13 13:02:45 +0900

    Add SecurityConfig

---
+++ src/main/kotlin/com/techie/ebanking/config/SecurityConfig.kt
@@ -0,0 +1,9 @@
+package com.techie.ebanking.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+
+@Configuration
+@EnableWebSecurity
+class SecurityConfig {
+}
